1a1907141374aa7467bba4968b8167b14e9a500c
vanillyn <108785622+vanillyn@users.noreply.github.com>
2022-11-14 10:30:21 -0500

    Update cuteanimeboy.js

--- wipcommands/cuteanimeboy.js
+++ wipcommands/cuteanimeboy.js
@@ -1,18 +1,18 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { gelkey, gelid } = require("../config.json");
+const { request } = require('undici');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('blush')
-		.setDescription('ahehe... *runs*')
+		.setName('cuteanimeboy')
+		.setDescription('get a cute anime boy from gelbooru!'),
 	async execute(interaction) {
-		const gif = Math.floor(Math.random() * gifs.length);
-		const embed = new EmbedBuilder()
-			.setColor(0xA5E9FF)
-			.setDescription(`${interaction.user} is embarrassed!`)
-			.setAuthor({ name: 'Vanillin' })
-			.setImage(gifs[gif])
-			.setTimestamp();
-		await interaction.reply({ embeds: [embed] });
+		const result = await request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=1boy+trap+-rating:explicit+-rating:questionable+-1girl+-2girls+-3girls+-4girls+-5girls+-child&json=1`)
+		const file = await result.body.json()
+		const gpost = file.post
+		console.log(file)
+		await interaction.reply({ content: `**finished!**\n\`id:\` ${gpost.id}\n\`tags:\` ${gpost.tags}\n\`source:\` <${gpost.source}>` file: gpost.file_url});
 	},
 };
