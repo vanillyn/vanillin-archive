4d7724473b374ec4ec75a707d9c1fa51b0609710
vanillyn <108785622+vanillyn@users.noreply.github.com>
2022-11-15 13:39:36 -0500

    Update cuteanimegirl.js

--- commands/cuteanimegirl.js
+++ commands/cuteanimegirl.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { gelkey, gelid } = require("../config.json");
+const { auth } = require("../config.json");
 const { request } = require('undici');
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
 		.setName('cuteanimegirl')
 		.setDescription('get a cute anime girl from gelbooru!'),
 	async execute(interaction) {
-		const result = await request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=sort:random+1girl+-rating:explicit+-rating:questionable+-1boy+-2boys+-3boys+-4boys+-5boys&json=1&api_key=${gelkey}&user_id=${gelid}`)
+		const result = await request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=sort:random+1girl+-rating:explicit+-rating:questionable+-1boy+-2boys+-3boys+-4boys+-5boys&json=1&api_key=${auth.gelkey}&user_id=${auth.gelid}`)
 		const file = await result.body.json()
 		const gpost = file.post[0]
 		console.log(file)
